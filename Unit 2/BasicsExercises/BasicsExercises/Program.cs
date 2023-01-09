ed301663732428fcd84d2e7d79c8f111e854b61d
Justin <Justin@grandcircus.co>
2023-01-09 13:59:27 -0500

    Class start :)

---
+++ Unit 2/BasicsExercises/BasicsExercises/Program.cs
@@ -0,0 +1,32 @@
+﻿//Ex 1
+Console.WriteLine("Enter some text:");
+string value = Console.ReadLine();
+Console.WriteLine(value);
+
+//Ex 2
+Console.WriteLine("Enter a number");
+int num = int.Parse(Console.ReadLine());
+num++;
+Console.WriteLine(num);
+
+//Ex 3
+Console.WriteLine("Enter a number");
+double num2 = double.Parse(Console.ReadLine());
+num2 +=  0.5;
+Console.WriteLine(num2);
+
+//Ex 4
+Console.WriteLine("Please enter a number");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a second number");
+double y = double.Parse(Console.ReadLine());
+double z = x + y;
+Console.WriteLine("The sum is " + z);
+Console.WriteLine(z);
+
+//Ex 5
+Console.WriteLine("Please enter a number");
+double value1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a second number");
+double value2 = double.Parse(Console.ReadLine());
+Console.WriteLine("The product is " + value1 * value2);
---
+++ Unit 2/day1Lecture/day1Lecture/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//This is a comment
+//Console.WriteLine("I am active");
+
+/* Author: Justin Jones
+  Date: 1/9/2023
+   hfdskjalhdasfjklhdkajfslhbiuydfeasghiu
+ */
+
+//x is being declared
+int x;
+//x is being intialized
+x = 10;
+
+//combined
+int y = 5;
+
+//constant
+const double TAXRATE = 0.06;
+//TAXRATE = 1; cannot change a constant
+
+//example
+
+Console.WriteLine("Please enter a radius");
+//string result = Console.ReadLine();
+//double num = double.Parse(result);
+double num = double.Parse(Console.ReadLine());
+double area = num * num * Math.PI;
+Console.WriteLine(area);
+
+short z = short.Parse(Console.ReadLine());
+//NULL
+string myName = null;
+Console.WriteLine(myName);
+string friend = "Josh Carolin";
+
+if(friend == "Family")
+{
+    myName = "Justin Jones";
+}
+else if (friend == "Josh Carolin")
+{
+    myName = "Jeo";
+}
+Console.WriteLine(myName);
+
