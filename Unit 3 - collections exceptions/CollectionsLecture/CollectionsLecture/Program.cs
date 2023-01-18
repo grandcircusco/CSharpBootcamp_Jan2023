c34416beba7901e5b1c68f93a64d4a613e5e29e0
Justin <Justin@grandcircus.co>
2023-01-18 14:18:28 -0500

    collections

---
+++ Unit 3 - collections exceptions/CollectionsLecture/CollectionsLecture/Program.cs
@@ -0,0 +1,143 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+//Generics
+//List
+//Once a list is created and has values, it works just like an array
+List<int> nums = new List<int>();
+nums.Add(5);
+nums.Add(10);
+nums.Add(9);
+nums.Add(11);
+Console.WriteLine(nums[1]);
+foreach(int i in nums)
+{
+    Console.WriteLine(i);
+}
+//List methods
+nums.Remove(9);//takes in object
+nums.RemoveAt(0);//takes in index
+Console.WriteLine($"there are {nums.Count} elements");//.Count returns how many elements
+foreach (int i in nums)
+{
+    Console.WriteLine(i);
+}
+
+//List with starting values
+List<string> names = new List<string>()
+{
+    "Justin",
+    "Josh",
+    "Billy"
+};
+
+
+//Dictionary
+//<key, value>
+Dictionary<string, int> phonebook = new Dictionary<string, int>();
+phonebook.Add("Purple Giraffe Real Estate", 0123456789);
+phonebook.Add("Cuzzo’s Candy Shop", 1116543210);
+phonebook.Add("Michaels Cat Cafe", 1223334444);
+
+Console.WriteLine(phonebook["Purple Giraffe Real Estate"]);
+
+foreach (KeyValuePair<string, int> kvp in phonebook)
+{
+    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+}
+
+//Exercises
+//47
+List<string> ex47 = new List<string>();
+while (true)
+{
+    Console.WriteLine("Enter text");
+    string input = Console.ReadLine();
+    ex47.Add(input);
+    string result = "";
+    foreach(string s in ex47)
+    {
+        result += s + " ";
+    }
+    Console.WriteLine(result);
+    Console.WriteLine("do you want to continue? y/n");
+    string choice = Console.ReadLine();
+    if(choice == "n")
+    {
+        break;
+    }
+}
+
+//48
+List<int> ex48 = new List<int>();
+while (true)
+{
+    Console.WriteLine("Please enter a number. (q to quit)");
+    string input = Console.ReadLine();
+    if(input == "q")
+    {
+        break;
+    }
+    else
+    {
+        int result = int.Parse(input);
+        ex48.Add(result);
+    }
+}
+string finalMath = "";
+foreach(int i in ex48)
+{
+    finalMath += i + "+";
+}
+finalMath = finalMath.Substring(0,finalMath.Length-1);
+Console.WriteLine($"{finalMath}={ex48.Sum()}");
+
+//51
+Dictionary<string, string> translation = new Dictionary<string, string>();
+translation.Add("hello", "hola");
+translation.Add("food", "comida");
+translation.Add("world", "mundo");
+translation.Add("computer", "computadora");
+translation.Add("exercise", "ejercicio");
+
+Console.WriteLine("Enter a word in English:");
+string english = Console.ReadLine();
+string spanish = translation[english];
+
+Console.WriteLine($"{english} in spanish is {spanish}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+//NON GENERICS
+//DO NOT USE OR JUSTIN WILL FIGHT YOU
+//Arraylist
+//ArrayList numbers = new ArrayList();
+//numbers.Add(5);
+//numbers.Add(10);
+//numbers.Add("justin");
+//numbers.Add(new ArrayList());
+//foreach (var i in numbers)
+//{
+//    Console.WriteLine(i);
+//}
+
+////Hashtable
+//Hashtable ht = new Hashtable();
+//ht.Add("Earle's Pizza Palace", 1234567890);
+//ht.Add("Belle's Noodle and Company", 0987654321);
+//ht.Add("Justin's Taco Bell", 1112223333);
+//ht.Add(true, "Josh's secret lab");
+
+//Console.WriteLine(ht["Earle's Pizza Palace"]);
---
+++ Unit 3 - collections exceptions/LinqLecture/LinqLecture/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> numbers = new List<int>()
+{
+    19,21,25,20,30
+};
+
+//Count method
+//returns how many match the condition
+int adultCount = numbers.Count(age => age >= 21);
+Console.WriteLine($"There are {adultCount} adults");
+
+//any method
+//return true or false for the condition
+bool adultCheck = numbers.Any(age => age >=21);
+Console.WriteLine($"Is there an adult here? {adultCheck}");
+//is anyone in the group 65 or older?
+bool seniorCheck = numbers.Any(age => age >= 65);
+Console.WriteLine($"Is there a senior here? {seniorCheck}");
+
+
+List<double> Clothes = new List<double>()
+{
+    75,100,250,300,90,100,350,90,100 
+};
+
+//Where method
+//Create a collection based on condition
+List<double> expensive = Clothes.Where(c => c >= 100).ToList();
+foreach(double e in expensive)
+{
+    Console.WriteLine(e);
+}
+//Bring back all prices that are equal to 100
+List<double> hundred = Clothes.Where(c => c == 100).ToList();
+foreach (double e in hundred)
+{
+    Console.WriteLine(e);
+}
