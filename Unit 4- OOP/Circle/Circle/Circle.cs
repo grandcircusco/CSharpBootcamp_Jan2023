ebbdf224422c1f54d03b4574b4ff4e70ec090a95
Justin <Justin@grandcircus.co>
2023-01-24 11:02:53 -0500

    circles

---
+++ Unit 4- OOP/Circle/Circle/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    public class CircleObj//Renamed class so c# can tell difference
+    {
+        //properties
+        public double Radius { get; set; }
+        //constructor
+        public CircleObj(double _radius)
+        {
+            Radius = _radius;
+        }
+
+        //methods
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius,2);
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x,2).ToString();
+        }
+
+    }
+}
---
+++ Unit 4- OOP/Circle/Circle/Program.cs
@@ -0,0 +1,36 @@
+﻿using Circle;
+
+Console.WriteLine("Welcome to the Circle Calculator.");
+
+//Counter
+//int circles = 0;
+List<CircleObj> allCircles = new List<CircleObj>();
+
+//program loop
+bool runProgram = true;
+while (runProgram)
+{
+    //getting user input
+    //Console.WriteLine("Please enter a number.");
+    double input = Validator.GetNumberInRangeDouble(0);
+
+    //Create circle object
+    CircleObj myCircle = new CircleObj(input);
+    //circles++;
+    allCircles.Add(myCircle);
+
+    Console.WriteLine($"Circumference: {myCircle.CalculateFormattedCircumference()}");
+    Console.WriteLine($"Area: {myCircle.CalculateFormattedArea()}");
+
+    //ask for continue
+    runProgram = Validator.GetContinue("Would you like to make more circles?","Yes", "No");
+    
+}
+
+//Console.WriteLine($"You created {circles} circles.");
+Console.WriteLine($"You created {allCircles.Count} circles.");
+
+foreach(CircleObj c in allCircles)
+{
+    Console.WriteLine($"Radius: {c.Radius} Area: {c.CalculateFormattedArea()} Circumference {c.CalculateFormattedCircumference()}");
+}
