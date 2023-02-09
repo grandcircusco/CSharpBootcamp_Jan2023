90440668014d40707bad131846ee217b5e71c299
Justin <Justin@grandcircus.co>
2023-02-09 15:53:42 -0500

    ASP

---
+++ Unit 7- Html, MVC APIS/ASP Lecture/ASP Lecture/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ASP_Lecture.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_Lecture.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+
+        //public IActionResult Result(string name, int age)
+        //{
+        //    string result = $"We have collected {name}. They are {age} years old";
+        //    return View(result);
+        //}
+
+        public IActionResult Result(Cat c)
+        {
+            return View(c);
+        }
+
+        public IActionResult About()
+        {
+            string name = "Justin";
+            ViewData["Name"] = name;
+            //Name is stored in the dictionary.
+            //ViewData can be used on the View
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            int Year = 2023;
+            return View(Year);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unit 7- Html, MVC APIS/ASP Lecture/ASP Lecture/Models/Cat.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP_Lecture.Models
+{
+    public class Cat
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
