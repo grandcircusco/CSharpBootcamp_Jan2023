5a20341726ded0e6ac10e59762b993420cf40839
Justin <Justin@grandcircus.co>
2023-02-15 14:14:34 -0500

    movie lab

---
+++ Unit 7- Html, MVC APIS/OMDB/OMDB/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDB.Models;
+using System.Diagnostics;
+
+namespace OMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieSearch(string title)
+        {
+            return View(MovieDAL.GetMovie(title));
+        }
+
+        [HttpGet]
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> result = new List<MovieModel>();
+            result.Add(MovieDAL.GetMovie(title1));
+            result.Add(MovieDAL.GetMovie(title2));
+            result.Add(MovieDAL.GetMovie(title3));
+
+            return View(result);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unit 7- Html, MVC APIS/OMDB/OMDB/Models/MovieDAL.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace OMDB.Models
+{
+    public class MovieDAL
+    {
+        public static MovieModel GetMovie(string title)
+        {
+            //setup
+            string key = Secret.apiKey;
+            string url = $"http://www.omdbapi.com/?apikey={key}&t={title}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+    }
+}
